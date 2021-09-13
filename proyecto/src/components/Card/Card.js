b1d9835e0ee22c84cfeccbd1c2d9beed128218d0
Mati <mgallardo@udesa.edu.ar>
2021-09-13 09:08:47 -0300

    loader bien

--- proyecto/src/components/Card/Card.js
+++ proyecto/src/components/Card/Card.js
@@ -12,7 +12,7 @@ export default class Card extends Component {
     }
 
 componentDidMount() {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US&page=1')
+    fetch('')
     .then(response => response.json())
     .then(data => { 
         
@@ -29,7 +29,7 @@ render () {
     console.log(this.state.contenido);
     return (
         <div> 
-            {this.state.contenido === []  ?
+            {this.state.contenido.length === 0  ?
             
             <img src={cargando} alt=''/> :
 
