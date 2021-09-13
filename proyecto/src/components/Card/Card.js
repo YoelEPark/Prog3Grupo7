91cc89fd22c5e96d65767dece43d0bf124179f14
Mati <mgallardo@udesa.edu.ar>
2021-09-13 10:19:32 -0300

    s

--- proyecto/src/components/Card/Card.js
+++ proyecto/src/components/Card/Card.js
@@ -12,7 +12,7 @@ export default class Card extends Component {
     }
 
 componentDidMount() {
-    fetch('')
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US&page=1')
     .then(response => response.json())
     .then(data => { 
         
